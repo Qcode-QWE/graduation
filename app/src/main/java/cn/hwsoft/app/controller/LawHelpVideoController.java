4b8d53e9046b848a65c2b40ec72f565be2ceca5a
chenjinpeng <jinpeng@ideamake.cn>
2020-04-14 21:32:36 +0800

    feat:add chatting room init for app

---
+++ app/src/main/java/cn/hwsoft/app/controller/LawHelpVideoController.java
@@ -0,0 +1,54 @@
+package cn.hwsoft.app.controller;
+
+import cn.hwsoft.wisdom.core.domain.Law_help_video_log;
+import cn.hwsoft.wisdom.core.query.JSONResult;
+import cn.hwsoft.wisdom.core.service.AdminService;
+import cn.hwsoft.wisdom.core.service.LawHelpVideoService;
+import cn.hwsoft.wisdom.core.service.UserService;
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 后台管理：咨询，援助，救助（列表查看，留言回复）
+ * Created by Lenovo on 2019/7/15.
+ */
+@RestController
+@RequestMapping("/law/help/video")
+public class LawHelpVideoController {
+    @Autowired
+    private LawHelpVideoService lawHelpVideoService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AdminService adminService;
+
+    @PostMapping
+    public JSONResult createLawHelpLog(String request) {
+        if (request != null && StringUtils.isEmpty(request)) {
+            return JSONResult.error("title params must not null!");
+        }
+
+        Law_help_video_log lawHelpVideoLog = JSON.parseObject(request, Law_help_video_log.class);
+        /**
+         * todo 获取当前用户id，name
+         */
+        lawHelpVideoLog.setUserId(10001);
+        lawHelpVideoLog.setUserName("陈小鱼");
+        boolean b = lawHelpVideoService.createLawHelpLog(lawHelpVideoLog);
+        return JSONResult.success(b);
+    }
+
+    @PutMapping
+    public JSONResult updateLawHelpLog(String request) {
+        Law_help_video_log lawHelpVideoLog = JSON.parseObject(request, Law_help_video_log.class);
+        boolean b = lawHelpVideoService.updateLawHelpLog(lawHelpVideoLog);
+        return JSONResult.success(b);
+    }
+
+
+}
---
+++ app/src/main/webapp/view/law/js/client.js
@@ -0,0 +1,230 @@
+'use strict'
+var localVideo = document.querySelector('video#localvideo');
+var remoteVideo= document.querySelector('video#remotevideo');
+
+var btnConn= document.querySelector('button#connserver');
+var btnLeave= document.querySelector('button#leave');
+
+var localStream = null;
+
+var roomid = '111111';
+var socket = null;
+
+var state = 'init';
+
+var pc = null;
+
+function sendMessage(roomid,data){
+    console.log('send p2p message',roomid,data);
+    if(socket){
+        socket.emit('message',roomid,data);
+    }
+}
+
+function handleOfferError(err){
+    console.error('Failed to get Offer!',err);
+}
+
+function handleAnswerError(err){
+    console.error('Failed to get Answer!',err);
+}
+
+function getAnswer(desc){
+    pc.setLocalDescription(desc);
+    sendMessage(roomid,desc);
+}
+
+function getOffer(desc){
+    pc.setLocalDescription(desc);
+    sendMessage(roomid,desc);
+}
+
+function call(){
+    if(state==='joined_conn'){
+        if(pc){
+            var options = {
+                offerToReceiveAudio:1,
+                offerToReceiveVideo:1
+            }
+            pc.createOffer(options)
+                .then(getOffer)
+                .catch(handleOfferError);
+        }
+    }
+}
+function conn(){
+    socket = io.connect("https://chenjinpeng.top:443");
+
+    socket.on('joined',(roomid,id)=>{
+        console.log('receive joined message:',roomid,id);
+        state = 'joined';
+        createPeerConnection();
+        btnConn.disabled = true;
+        btnLeave.disabled = false;
+
+        console.log('receive joined message:state=',state);
+    });
+
+    socket.on('otherjoin',(roomid,id)=>{
+        console.log('receive otherjoin message:',roomid,id);
+
+        if(state === 'joined_unbind'){
+            createPeerConnection();
+        }
+        state = 'joined_conn';
+        console.log('receive otherjoin message:state=',state);
+        call();
+    });
+
+    socket.on('full',(roomid,id)=>{
+        console.log('receive full message:',roomid,id);
+        state = 'leaved';
+        console.log('receive full message:state=',state);
+        socket.disconnect();
+        alert('the room is full!');
+    });
+
+    socket.on('leaved',(roomid,id)=>{
+        console.log('receive leaved message:',roomid,id);
+        state = 'leaved';
+        console.log('receive leaved message:state=',state);
+        socket.disconnect();
+
+        btnConn.disabled = false;
+        btnLeave.disbaled = true;
+    });
+
+    socket.on('bye',(roomid,id)=>{
+        console.log('receive bye message:',roomid,id);
+        state = 'joined_unbind';
+        closePeerConnection();
+        console.log('receive bye message:state=',state);
+    });
+
+    socket.on('message',(roomid,data)=>{
+        console.log('receive message message:',roomid,data);
+
+        if(data){
+            if(data.type === 'offer'){
+                pc.setRemoteDescription(new RTCSessionDescription(data));
+                pc.createAnswer()
+                    .then(getAnswer)
+                    .catch(handleAnswerError);
+            }else if(data.type === 'answer'){
+                pc.setRemoteDescription(new RTCSessionDescription(data));
+            }else if(data.type === 'candidate'){
+                var candidate  = new RTCIceCandidate({
+                    sdpMLineIndex:data.label,
+                    candidate:data.candidate
+                });
+
+                pc.addIceCandidate(candidate);
+            }else{
+                console.error('the message is invalid!',data);
+            }
+        }
+    });
+
+    socket.emit('join','111111');
+    return ;
+
+}
+
+function getMediaStream(stream){
+    localStream  =stream;
+    localVideo.srcObject = localStream;
+
+    conn();
+}
+
+function handleError(err){
+    if(err){
+        console.error('Failed to get Media Stream:',err);
+    }
+}
+
+function start(){
+    if(!navigator.mediaDevices ||
+        !navigator.mediaDevices.getUserMedia){
+        console.error('ths getUserMedia is not support!');
+        return;
+    }else{
+        var constraints = {
+            video : true,
+            audio: true
+        }
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(getMediaStream)
+            .catch(handleError);
+    }
+}
+
+function connSignalServer(){
+    alert("chong");
+    start();
+    return true;
+}
+
+function closeLocalMedia(){
+    if(localStream && localStream.getTracks()){
+        localStream.getTracks().forEach((track)=>{
+            track.stop();
+        });
+    }
+    localStream = null;
+}
+function leave(){
+    if(socket){
+        socket.emit('leave','111111');
+    }
+    closePeerConnection();
+    closeLocalMedia();
+    btnConn.disabled = false;
+    btnLeave.disable = true;
+}
+
+function createPeerConnection(){
+    console.log('create RTCPeerConnection!');
+    if(!pc){
+        var pcConfig = {
+            'iceServers':[{
+                'urls':'turn:114.116.238.215:3478',
+                'credential':'123456',
+                'username':'root'
+            }]
+        }
+        pc = new RTCPeerConnection(pcConfig);
+        pc.onicecandidate = (e)=>{
+            if(e.candidate){
+                console.log('find an new candidate',e.candidate);
+                sendMessage(roomid,{
+                    type:'candidate',
+                    label:e.candidate.sdpMLineIndex,
+                    id:e.candidate.sdpMid,
+                    candidate:e.candidate.candidate
+                })
+            }
+        }
+
+        pc.ontrack = (e)=>{
+            remoteVideo.srcObject = e.streams[0];
+        }
+    }
+
+    if(localStream){
+        localStream.getTracks().forEach((track)=>{
+            pc.addTrack(track,localStream);
+        });
+    }
+}
+
+function closePeerConnection(){
+    console.log('close RTCPeerConnection!');
+    if(pc){
+        pc.close();
+        pc = null;
+    }
+}
+btnConn.onclick = connSignalServer
+btnLeave.onclick = leave;
